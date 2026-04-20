b1e356d2c3b65e402ea8102a83ec00425e67ac16
ghostfree <kain7402@naver.com>
2026-04-20 16:29:21 +0900

    텍스트 수정

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -15,7 +15,7 @@ function getTagColorByIndex(index) {
 const projects = [
     {
         num: '04',
-        title: 'BC카드 WIZE-BIZ 구축[04.27 오픈예정]',
+        title: 'BC카드 와이즈비즈 리뉴얼[04.27 오픈예정]',
         desc: 'Nunjucks 구조로 화면 컴포넌트를 재설계해 유지보수성과 확장성을 높인 구축 프로젝트입니다.',
         tags: ['Nunjucks', 'Gulp', 'Sass', 'JavaScript', '웹접근성 마크획득(Web Watch)'],
         link: 'https://wisebiz.bccard.com',
